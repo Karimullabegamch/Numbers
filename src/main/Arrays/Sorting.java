441137a87cf057489fc85b893a55d4b427117e49
Karimullabegamch <94620126+Karimullabegamch@users.noreply.github.com>
2024-08-01 16:46:55 +0000

    Numbers and Arrays

---
+++ src/main/Arrays/Sorting.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Sorting {
+  public static void main(String[] args) {
+    int a[] = { 2, 33, 1, 4, 3 };
+    System.out.println(Arrays.toString(a));
+    Arrays.sort(a);
+    System.out.println(Arrays.toString(a));
+  }
+}
--- src/main/Numbers/FibonaciSeries.java
+++ src/main/Numbers/FibonaciSeries.java
@@ -1,10 +1,10 @@
-import java.util.Scanner;
+
 public class FibonaciSeries {
   public static void main(String[] args) {
    
     int a=0,b=1,c=0;
     for (int i = 1; i <= 5; i++) {
-      System.out.println(a);
+      System.out.print(a+" ");
       c=a+b;
       a=b;
       b=c;
