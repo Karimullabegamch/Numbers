1ffaf5fb0d64f72fc806d4f6fb839ecc91dad8fd
Karimullabegamch <94620126+Karimullabegamch@users.noreply.github.com>
2024-08-01 16:56:39 +0000

    Increment

---
+++ src/main/BasicsOfJava/Incr.java
@@ -0,0 +1,12 @@
+public class Sorting {
+  public static void main(String[] args) {
+
+    int a = 3;
+    System.out.println(a);
+    System.out.println(a++);
+    System.out.println(a);
+    System.out.println(a);
+    System.out.println(a+1);
+    System.out.println(a);
+  }
+}
