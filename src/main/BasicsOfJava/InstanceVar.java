1470a414e7c726c98dfcdba7ac904e35f856c57e
Karimullabegamch <94620126+Karimullabegamch@users.noreply.github.com>
2024-08-02 02:49:03 +0000

    Instance Variables

--- src/main/BasicsOfJava/InstanceVar.java
+++ src/main/BasicsOfJava/InstanceVar.java
@@ -1,19 +1,22 @@
 public class InstanceVar {
   int x=100;
 
-  void meth1()
+  void meth1(InstanceVar obj)
   {
-    System.out.println(x);
+    System.out.println(x+obj.x);
     System.out.println(this.x);
-    InstanceVar obj=new InstanceVar();
-    System.out.println(obj.x);
+    InstanceVar obj1=new InstanceVar();
+    System.out.println(obj1.x);
   }
   public static void main(String[] args) {
 
     InstanceVar obj=new InstanceVar();
-    System.out.println(obj.x);
-    obj.meth1();
+    System.out.println(obj.x);//100
+    obj.meth1(obj);//200 ,100,100,
     obj.x=99;
-     System.out.println(obj.x);
-    obj.meth1();
+     System.out.println(obj.x);//99
+    obj.meth1(obj);//198,99,100
   }}
+
+
+ In instance variable once object is created and modified that value so that modified value will be continued upto we created a new object
