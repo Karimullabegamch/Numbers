42ea9c9b71273cbd7002a54bf812e1c04b252c3c
Karimullabegamch <94620126+Karimullabegamch@users.noreply.github.com>
2024-08-03 18:04:32 +0000

    Hashcode

---
+++ src/main/BasicsOfJava/HashCode.java
@@ -0,0 +1,11 @@
+public class HashCode {
+  public static void main(String[] args) {
+
+    long a = 10;
+    long b = 20;
+    long c = 10;
+    System.out.println("a= " + a.hashCode());
+    System.out.println("b= " + b.hashCode());
+    System.out.println("c= " + c.hashCode());
+  }
+}
