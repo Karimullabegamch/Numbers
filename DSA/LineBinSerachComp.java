57d8935500de8391176d06fe456e10460350a524
Karimullabegamch <94620126+Karimullabegamch@users.noreply.github.com>
2024-09-01 16:38:10 +0000

    Binary and Linear search comparision
    binary search  --> O(log n)
    linear Search --> O(n)

---
+++ DSA/LineBinSerachComp.java
@@ -0,0 +1,56 @@
+public class LineBinSerachComp {
+ public static int binarySearch(int arr[],int key)
+    {
+      int left=0;
+      int right=arr.length-1;
+     int bcount=0;
+   while(left<=right)
+        {
+         int mid=(left+right)/2;
+          bcount++;
+          if(arr[mid]==key)
+          {
+            System.out.println("binary count "+bcount);
+          
+            return mid;
+          }
+          else if(arr[mid]<key)
+            left=mid+1;
+          else
+            right=mid-1;
+        }
+       System.out.println("binary count "+bcount);
+      return -1;
+
+    }
+  public static int linearSearch(int arr[],int key)
+    {
+      int lcount=0;
+      for(int i=0;i<arr.length;i++)
+        {
+          lcount++;
+          if(arr[i]==key)
+          {
+             System.out.println("linear count "+lcount);
+            return i;
+          }
+        }
+      System.out.println("linear count "+lcount);
+      return -1;
+
+    }
+
+    public static void main(String[] args)
+    {
+      int arr[]={3,4,5,6,7,86};
+      int target=77;
+
+      int res=linearSearch(arr,target);
+
+      int res1=binarySearch(arr,target);
+      if(res !=-1)
+        System.out.println("element found at index "+res);
+      else
+        System.out.println("elemetn not found");
+    }
+  }
