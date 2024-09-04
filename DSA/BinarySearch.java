9c8789105116ce0ea0b95cbd811e305a41283467
Karimullabegamch <94620126+Karimullabegamch@users.noreply.github.com>
2024-09-04 11:44:08 +0000

    binary serach

--- DSA/BinarySearch.java
+++ DSA/BinarySearch.java
@@ -2,19 +2,19 @@ public class BinarySearch
   {
  public static int binarySearch(int arr[],int key)
     {
-      int left=0;
-      int right=arr.length-1;
+      int start=0;
+      int end=arr.length-1;
    
-   while(left<=right)
+   while(start<=end)
         {
-         int mid=(left+right)/2;
+         int mid=(start+end)/2;
           
           if(arr[mid]==key)
             return mid;
           else if(arr[mid]<key)
-            left=mid+1;
+            start=mid+1;
           else
-            right=mid-1;
+            end=mid-1;
         }
       return -1;
 
