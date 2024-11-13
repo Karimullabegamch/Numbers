5642d5d641a3a668b4887be131b24a6223d8e9fa
Karimullabegamch <94620126+Karimullabegamch@users.noreply.github.com>
2024-11-13 09:32:54 +0000

    Min and max values

---
+++ java/main/BasicsOfJava/MinMax.java
@@ -0,0 +1,16 @@
+public class MinMax
+  {
+
+    public static void main (String [] args)
+    {
+      System.out.println("min, max and size");
+      System.out.println("Byte");
+      System.out.println("min value "+ Byte.MIN_VALUE);
+      System.out.println("max value "+ Byte.MAX_VALUE);
+      System.out.println("size in bits "+ Byte.SIZE);
+      System.out.println("short");
+      System.out.println("min value "+ Short.MIN_VALUE);
+      System.out.println("max value "+ Short.MAX_VALUE);
+      System.out.println("size in bits "+ Short.SIZE);
+    }
+  }
